dcfdd5b3c15b2f674df2e57a07b61b70b1061769
Daniel J. Rollins <daniel.rollins@infinityworks.com>
2018-12-18 00:04:03 +0000

    finish christmas challenge

--- christmas-challenge/src/main.rs
+++ christmas-challenge/src/main.rs
@@ -1,18 +1,22 @@
-use std::hash::{Hash, Hasher}; use std::ops::Deref;
 use std::cmp::Ordering;
+use std::collections::HashSet;
 use std::error::Error;
+use std::f64::{INFINITY, MIN, NEG_INFINITY};
+use std::hash::{Hash, Hasher};
 use std::io::{self, Read};
+use std::ops::Deref;
 use std::str::FromStr;
-use std::f64::{INFINITY, NEG_INFINITY};
 
-use plotlib::scatter::{self, Scatter};
-use plotlib::view::View;
-use plotlib::page::Page;
-use plotlib::style::{Point, Marker};
+//use plotlib::page::Page;
+//use plotlib::scatter::{self, Scatter};
+//use plotlib::style::{Marker, Point};
+//use plotlib::view::View;
+
+use rayon::prelude::*;
 
 type Id = u32;
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone)]
 struct Record {
     id: Id,
     lat: NotNan,
@@ -26,6 +30,13 @@ impl Hash for Record {
     }
 }
 
+impl PartialEq for Record {
+    fn eq(&self, other: &Record) -> bool {
+        self.id == other.id
+    }
+}
+
+impl Eq for Record {}
 
 #[derive(Debug, PartialEq, PartialOrd, Clone, Copy)]
 struct NotNan {
@@ -44,18 +55,30 @@ impl FromStr for NotNan {
     type Err = Box<Error>;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        NotNan::from_f64(s.parse()?).ok_or(Box::<Error>::from("was NaN"))
+        NotNan::from_f64(s.parse()?).ok_or_else(|| Box::<Error>::from("was NaN"))
     }
 }
 
 impl NotNan {
     fn from_f64(as_f64: f64) -> Option<Self> {
-        if as_f64.is_nan() { None } else { Some(NotNan { as_f64 }) }
+        if as_f64.is_nan() {
+            None
+        } else {
+            Some(NotNan { as_f64 })
+        }
     }
 
-    fn infinity() -> Self { NotNan::from_f64(INFINITY).unwrap() }
+    fn infinity() -> Self {
+        NotNan::from_f64(INFINITY).unwrap()
+    }
 
-    fn neg_infinity() -> Self { NotNan::from_f64(NEG_INFINITY).unwrap() }
+    fn neg_infinity() -> Self {
+        NotNan::from_f64(NEG_INFINITY).unwrap()
+    }
+
+    fn min() -> Self {
+        NotNan::from_f64(MIN).unwrap()
+    }
 }
 
 impl Eq for NotNan {}
@@ -69,11 +92,12 @@ impl Ord for NotNan {
     }
 }
 
-fn flat_distance((lat1, lon1): (f64, f64), (lat2, lon2): (f64, f64)) -> NotNan {
-    NotNan::from_f64((lat1 - lat2).abs().hypot((lon1 - lon2).abs())).unwrap()
-}
+fn hav_distance((lat1, lon1): (NotNan, NotNan), (lat2, lon2): (NotNan, NotNan)) -> NotNan {
+    let lat1 = *lat1;
+    let lat2 = *lat2;
+    let lon1 = *lon1;
+    let lon2 = *lon2;
 
-fn hav_distance((lat1, lon1): (f64, f64), (lat2, lon2): (f64, f64)) -> NotNan {
     let r = 6372.8;
     let d_lat = (lat2 - lat1).to_radians();
     let d_lon = (lon2 - lon1).to_radians();
@@ -103,75 +127,105 @@ fn read_input() -> Result<Vec<Record>, Box<Error>> {
     Ok(records)
 }
 
-//fn solve_by_nearest(records: &[Record]) -> Vec<Id> {
-//    let mut result = vec![records[1].clone()];
-//    let mut records: HashSet<_> = records[1..].iter().collect();
-//
-//    loop {
-//        let nearest = records.iter().min_by(|x, y| {
-//            let record = result.last().unwrap();
-//            let x_dist = hav_distance((*record.lat, *record.lon), (*x.lat, *x.lon));
-//            let y_dist = hav_distance((*record.lat, *record.lon), (*y.lat, *y.lon));
-//
-//            x_dist.cmp(&y_dist)
-//        });
-//
-//        match nearest {
-//            None => return result.iter().map(|x| x.id).collect(),
-//            Some(&nearest) => {
-//                result.push(nearest.clone());
-//                records.remove(nearest);
-//            },
-//        }
-//
-//    }
-//}
+fn solve_by_nearest(records: &[Record]) -> Vec<Id> {
+    let mut records: HashSet<Record> = records.iter().cloned().collect();
+    let mut coord = (NotNan::min(), NotNan::min());
+    let mut result = Vec::new();
 
-const GRID_SIZE: usize = 10;
+    loop {
+        let nearest = records.iter().min_by(|x, y| {
+            let x_dist = hav_distance(coord, (x.lat, x.lon));
+            let y_dist = hav_distance(coord, (y.lat, y.lon));
 
-fn chunk(mut coords: Vec<Record>) -> Vec<Vec<Record>> {
-    let min_max = ((NotNan::infinity(), NotNan::infinity()), (NotNan::neg_infinity(), NotNan::neg_infinity()));
+            x_dist.cmp(&y_dist)
+        });
 
-    let ((min_lat, min_lon), (max_lat, max_lon)) = coords.iter().fold(min_max, |(min, max), coord| {
-        let min_lat = std::cmp::min(coord.lat, min.0);
-        let min_lon = std::cmp::min(coord.lon, min.1);
-        let max_lat = std::cmp::max(coord.lat, max.0);
-        let max_lon = std::cmp::max(coord.lon, max.1);
-        ((min_lat, min_lon), (max_lat, max_lon))
-    });
+        let nearest = match nearest {
+            None => return result,
+            Some(nearest) => nearest.clone(),
+        };
+
+        let nearest = records.take(&nearest).unwrap();
+        result.push(nearest.id);
+        coord = (nearest.lat, nearest.lon);
+    }
+}
+
+const GRID_SIZE: usize = 5;
+
+fn chunk(coords: Vec<Record>) -> Vec<Vec<Record>> {
+    let min_max = (
+        (NotNan::infinity(), NotNan::infinity()),
+        (NotNan::neg_infinity(), NotNan::neg_infinity()),
+    );
+
+    let ((min_lat, min_lon), (max_lat, max_lon)) =
+        coords.iter().fold(min_max, |(min, max), coord| {
+            (
+                (
+                    std::cmp::min(coord.lat, min.0),
+                    std::cmp::min(coord.lon, min.1),
+                ),
+                (
+                    std::cmp::max(coord.lat, max.0),
+                    std::cmp::max(coord.lon, max.1),
+                ),
+            )
+        });
 
     let max_lat_norm = *max_lat - *min_lat;
     let max_lon_norm = *max_lon - *min_lon;
 
-    coords.into_iter().fold(vec![Vec::new(); GRID_SIZE * GRID_SIZE], |mut grid, record| {
-        let lat_norm = *record.lat - *min_lat;
-        let lon_norm = *record.lon - *min_lon;
-        let lat_bucket = ((GRID_SIZE as f64) * (lat_norm / max_lat_norm)).floor() as usize;
-        let lon_bucket = ((GRID_SIZE as f64) * (lon_norm / max_lon_norm)).floor() as usize;
-        grid[lat_bucket + lon_bucket].push(record);
-        grid
-    })
+    coords.into_iter().fold(
+        vec![Vec::new(); GRID_SIZE * GRID_SIZE],
+        |mut grid, record| {
+            let lat_norm = *record.lat - *min_lat;
+            let lon_norm = *record.lon - *min_lon;
+            let lat_bucket = ((GRID_SIZE as f64) * (lat_norm / max_lat_norm)).floor() as usize;
+            let lon_bucket = ((GRID_SIZE as f64) * (lon_norm / max_lon_norm)).floor() as usize;
+            grid[lat_bucket * lon_bucket].push(record);
+            grid
+        },
+    )
 }
 
 fn main() -> Result<(), Box<Error>> {
     let input = read_input()?;
-    
-    let colours = ["red", "blue", "green"];
-    let mut colours_iter = colours.iter().cycle();
 
-    let view = View::new();
+    let data: Vec<_> = chunk(input)
+        .par_iter()
+        .rev()
+        .flat_map(|chunk| solve_by_nearest(chunk))
+        .collect();
 
-    let data: Vec<_> = chunk(input).into_iter().map(|chunk| {
-        let coords: Vec<_> = chunk.iter().map(|record| (*record.lat, *record.lon)).collect();
-        Scatter::from_vec(&coords[..])
-            .style(scatter::Style::new().colour(colours_iter.next().unwrap().clone()))
-    }).collect();
+    //    let colours = ["red", "blue", "green"];
+    //    let mut colours_iter = colours.iter().cycle();
 
-    let view = data.iter().fold(view, |view, data| {
-        view.add(data)
-    });
+    //    let view = View::new();
 
-    Page::single(&view).save("out2.svg");
+    //    let data: Vec<_> = chunk(input)
+    //        .into_iter()
+    //        .map(|chunk| {
+    //            let coords: Vec<_> = chunk
+    //                .iter()
+    //                .map(|record| (*record.lat, *record.lon))
+    //                .collect();
+    //            Scatter::from_vec(&coords[..]).style(
+    //                scatter::Style::new()
+    //                    .colour(colours_iter.next().unwrap().clone())
+    //                    .marker(Marker::Square)
+    //                    .size(0.5f32),
+    //            )
+    //        })
+    //        .collect();
+
+    //    let view = data.iter().fold(view, |view, data| view.add(data));
+
+    //    Page::single(&view).save("out2.svg");
+
+    for id in data {
+        println!("{}", id);
+    }
 
     Ok(())
 }
